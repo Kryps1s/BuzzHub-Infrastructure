08e8a96c31c1bcfbb186d9d807806af04670bc72
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-14 10:12:08 -0400

    layer bucket and attachment to lambda

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_version = "1.5.1"
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-     version = "5.4.0"
-   }
-   archive = {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.4.0"
+    }
+    archive = {
       source = "hashicorp/archive"
       version = "2.4.0"
-   }
- }
- backend "s3" {
+    }
+  }
+  backend "s3" {
     bucket         	   = "terraform-8zmqsm"
     key                = "terraform.tfstate"
     region         	   = "ca-central-1"
@@ -23,6 +23,3 @@ provider "aws" {
   region = "ca-central-1"
   shared_credentials_files = ["$HOME/.aws/credentials"]
 }
-
-
-
