11ebe0a53d1ca1a2d007679a5afeeea7f335ed75
Elliot O'Reilly <eoreilly1994@gmail.com>
2023-07-20 11:13:53 -0400

    importing state resources

--- state.tf
+++ state.tf
@@ -1,23 +1,33 @@
-# resource "aws_s3_bucket" "terraform-8zmqsm" {
-#    bucket = "terraform-8zmqsm"
-#    acl = "private"  
-# }
+resource "aws_s3_bucket" "terraform_state" {
+   bucket = "terraform-8zmqsm"
+}
 
-# resource "aws_s3_bucket_public_access_block" "terraform-8zmqsm" {
-#   bucket = aws_s3_bucket.terraform-8zmqsm.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_ownership_controls" "terraform_state_acl" {
+  bucket = "terraform-8zmqsm"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.terraform_state_acl]
 
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
 
-# resource "aws_dynamodb_table" "terraform-lock" {
-#   name         = "terraform-lock"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform-lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
